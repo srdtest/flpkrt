75a59a487a5266559c2c6998025ae5d411e9da4f
srdtest <sriramdeshmukh1983@gmail.com>
2025-03-08 00:39:16 -0600

    sds

---
+++ java/src/test/java/New_JAva_Project/java/second.java
@@ -0,0 +1,10 @@
+package New_JAva_Project.java;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("fdsfsdfsdfhsdfsdfhsdfiuosdhf");
+	}
+
+}

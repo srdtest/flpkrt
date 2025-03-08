8791a8d9cc651b461b035d6526562832c4c560ec
srdtest <sriramdeshmukh1983@gmail.com>
2025-03-08 00:00:59 -0600

    first commit

---
+++ java/src/main/java/New_JAva_Project/java/App.java
@@ -0,0 +1,13 @@
+package New_JAva_Project.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java/src/test/java/New_JAva_Project/java/firstjava.java
@@ -0,0 +1,12 @@
+package New_JAva_Project.java;
+
+public class firstjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello wordld");
+	}
+
+}
